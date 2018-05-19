b61bdfe42a6c3c1b92c7dc1cc531e3ea2f27e4e2
Robin Drexler <drexler.robin@gmail.com>
2018-05-19 12:56:39 +0200

    feat: make api more simple

--- react-snappy-selection/index.js
+++ react-snappy-selection/index.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
-const onClick = (e) => {
+const onClick = (event) => {
   if (!window.getSelection) {
     return;
   }
   const selection = window.getSelection();
-
   const range = selection.getRangeAt(0);
+  const target = event.target;
 
-  let target = e.target;
   if (range.commonAncestorContainer.contains(target)) {
     window.getSelection().selectAllChildren(target);
   }
 };
 
-export default ({ text, containerComponent: ContainerComponent = 'span' }) => (
-  <ContainerComponent onClick={onClick}>{text}</ContainerComponent>
-);
+const SnappyText = ({ children }) => {
+  if (typeof children === 'string') {
+    return <span onClick={onClick}>{children}</span>;
+  }
+
+  return React.Children.only(
+    React.cloneElement(children, {
+      onClick(event, ...args) {
+        typeof children.props.onClick === 'function' &&
+          children.props.onClick(event);
+        onClick(event);
+      }
+    })
+  );
+};
+
+export default SnappyText;
