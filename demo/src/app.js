44499722c9635f42b2451a7c975c09e43c820019
Robin Drexler <drexler.robin@gmail.com>
2018-05-19 12:57:02 +0200

    demo: adjust demo to new api

--- demo/src/app.js
+++ demo/src/app.js
@@ -1,14 +1,20 @@
-import React from 'react';
 import { render } from 'hops-react';
+import React from 'react';
 import SnappySelection from 'react-snappy-selection';
 
 const App = () => (
   <div>
     <span>This is not snappy</span>
-    <span>
-      <SnappySelection text="This is snappy!" />
-    </span>
+    <SnappySelection>This is snappy</SnappySelection>
     <span>This is not snappy</span>
+    <SnappySelection>
+      <span style={{ color: 'red' }}>This is also snappy</span>
+    </SnappySelection>
+    <SnappySelection>
+      <span style={{ color: 'blue' }} onClick={() => console.log('clicked')}>
+        This is also snappy and has onclick
+      </span>
+    </SnappySelection>
   </div>
 );
 
